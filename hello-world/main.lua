846ff06884caf4ecf3bb13869a50a32585206d97
Aly <aly.kazani52@gmail.com>
2025-08-02 16:23:00 +0500

    lua - hello world(2)

--- hello-world/main.lua
+++ hello-world/main.lua
@@ -7,3 +7,9 @@ print(A)
 
 -- or
 print "hello world! (but without parentheses)"
+
+-- or 
+print("hello" .." world!" .. " (but using concatenation)")
+
+-- or
+io.write("hello world! (but using stdout)")
